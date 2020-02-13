c42987d33eae8b42ed24fe2513d265d307e03981
Typhaine LE TROCQUER <letrocquer.typhaine@gmail.com>
2020-02-13 14:32:40 +0100

    modif 14h32 (13.02.20)
    ajout carousel vue.js
    modif grid page

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+import {
+    Hooper,
+    Slide,
+    Navigation as HooperNavigation
+} from 'hooper';
+
+export default {
+    components: {
+        Hooper,
+        Slide,
+        HooperNavigation
+    }
+}
